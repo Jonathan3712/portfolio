a68433000c82d1bf08c9b59bad49a6981cc1c50d
Jonathan <60109648+Jonathan3712@users.noreply.github.com>
2024-06-30 14:59:16 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -91,6 +91,21 @@ const scrollActive = () =>{
 		}                                                    
 	})
 }
-window.addEventListener('scroll', scrollActive)
+window.addEventListener('scroll', scrollActive) 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    // reset: true  // Animation Repeat 
+})
+
+sr.reveal('.home__data, .experience, .skilld, .contact__container')
+sr.reveal('home__img', {delay: 600})
+sr.reveal('home__scroll', {delay: 800})
+sr.reveal('work__card', '.service__card', {interval: 100})
+sr.reveal('about__content', {origin: 'right'})
+sr.reveal('about__img', {origin: 'left'})
